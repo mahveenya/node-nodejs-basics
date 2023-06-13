a5c8f7fcfcb4b45516c143a649020b51d548941e
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-13 17:30:07 +0300

    add: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,11 +3,11 @@ import fs from 'fs';
 const text = 'I am fresh and young'
 
 const create = async () => {
-  if(fs.existsSync('./files/fresh.txt')) {
+  if(fs.existsSync('./src/fs/files/fresh.txt')) {
     throw new Error('FS operation failed')
   } else {
-    fs.writeFile('./files/fresh.txt', text, (err) => {
-      err? console.log(err) : null
+      fs.writeFile('./src/fs/files/fresh.txt', text, (err) => {
+        err? console.log(err) : null
     })
   }
 };
