be3a234b9091f615e75c5f927cc1b47ae29654b3
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-14 10:51:13 +0300

    feat: add env func

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
-const parseEnv = () => {
-    // Write your code here 
-};
+import process from 'process'
 
-parseEnv();
+const parseEnv = () => {
+  process.env.RSS_ID = 123
+  process.env.RSS_PASSWORD = 123456
+  process.env.RSS_ZIP = 123321
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.includes('RSS_')) console.log(`${key}=${value}`)
+  }
+}
+
+parseEnv()
