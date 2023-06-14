3c4c141be1d38b53d3952a5e4b82b0836dae864e
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-14 16:41:14 +0300

    refactor: delete testing vars frim env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,9 +1,6 @@
 import process from 'process'
 
 const parseEnv = () => {
-  process.env.RSS_ID = 123
-  process.env.RSS_PASSWORD = 123456
-  process.env.RSS_ZIP = 123321
   for (const [key, value] of Object.entries(process.env)) {
     if (key.includes('RSS_')) console.log(`${key}=${value}`)
   }
