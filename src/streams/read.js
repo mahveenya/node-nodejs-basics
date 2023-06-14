cbee4c481600290ebaa54f70c8b0806f0250b120
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-15 00:27:32 +0300

    refactor: use pipe in read and write streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,9 +3,7 @@ import fs from 'fs'
 const rs = fs.createReadStream('./src/streams/files/fileToRead.txt')
 
 const read = async () => {
-  rs.on('data', chunk => {
-    process.stdout.write(chunk)
-  })
+  rs.pipe(process.stdout)
 }
 
 await read()
--- src/streams/write.js
+++ src/streams/write.js
@@ -3,20 +3,11 @@ import fs from 'fs'
 const ws = fs.createWriteStream('./src/streams/files/fileToWrite.txt')
 
 const displayInfo = () => {
-  process.stdout.write('Hello, dear colleague!')
-  process.stdout.write('\nIn order to check this task,')
-  process.stdout.write('\nplease enter any text into terminal,')
-  process.stdout.write('\nin order to save the entered line hit "Enter",')
-  process.stdout.write('\notherwise it won\'t be saved,')
-  process.stdout.write('\nto stop program execution use "Ctrl + C"')
-  process.stdout.write('\nand check fileToWrite.txt')
-  process.stdout.write("\n(I hope you'll find entered text there)\n")
+  process.stdout.write('Enter text, hit "Enter", check fileToWrite.txt\n')
 }
 
 const write = async () => {
-  process.stdin.on('data', (data) => {
-    ws.write(data)
-  })
+  process.stdin.pipe(ws)
 }
 
 displayInfo()
