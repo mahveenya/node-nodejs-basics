bb6761f21ae951701b8d703ee6f3fb16af38865b
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-15 21:53:11 +0300

    refactor: rewrite copy.js without try/catch

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,23 +4,11 @@ fs.access('./src/fs/files')
   .then(() =>
     fs
       .access('./src/fs/files_copy')
-      .then(() => {
-        try {
-          throw new Error('FS operation failed')
-        } catch (e) {
-          console.log(e)
-        }
-      })
-      .catch(copy)
+      .then(() => console.log(new Error('FS operation failed')))
+      .catch(() => copy())
   )
-  .catch(() => {
-    try {
-      throw new Error('FS operation failed')
-    } catch (e) {
-      console.log(e)
-    }
-  })
+  .catch(() => console.log(new Error('FS operation failed')))
 
 const copy = () => {
-  fs.cp('./src/fs/files', './src/fs/files_copy', { recursive: true, force: true })
+  fs.cp('./src/fs/files', './src/fs/files_copy', { recursive: true })
 }
