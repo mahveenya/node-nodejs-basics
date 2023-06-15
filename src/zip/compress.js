0f0aa905f70eef8851e605a9a1b1eb8c11126998
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-15 23:21:51 +0300

    refactor: encapsulate write stream in compress and decompress funcs

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,10 +2,11 @@ import fs from 'fs'
 import zlib from 'zlib'
 
 const rs = fs.createReadStream('./src/zip/files/fileToCompress.txt')
+const ws = fs.createWriteStream('./src/zip/files/archive.gz')
 const gzip = zlib.createGzip()
 
 const compress = async () => {
-  rs.pipe(gzip).pipe(fs.WriteStream('./src/zip/files/archive.gz'))
+  rs.pipe(gzip).pipe(ws)
 }
 
 await compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,10 +2,11 @@ import fs from 'fs'
 import zlib from 'zlib'
 
 const rs = fs.createReadStream('./src/zip/files/archive.gz')
+const ws = fs.createWriteStream('./src/zip/files/fileToCompress.txt')
 const gzip = zlib.createUnzip()
 
 const decompress = async () => {
-  rs.pipe(gzip).pipe(fs.WriteStream('./src/zip/files/fileToCompress.txt'))
+  rs.pipe(gzip).pipe(ws)
 }
 
 await decompress()
