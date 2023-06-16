3c0c6d1b598e6fe9bcbf12e628500387be9e6b2c
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-16 13:51:16 +0300

    refactor: rewrite copy.js with nested try/catch

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,14 +1,20 @@
-import fs from 'fs/promises'
+import { cp, access } from 'fs/promises'
 
-fs.access('./src/fs/files')
-  .then(() =>
-    fs
-      .access('./src/fs/files_copy')
-      .then(() => console.log(new Error('FS operation failed')))
-      .catch(() => copy())
-  )
-  .catch(() => console.log(new Error('FS operation failed')))
-
-const copy = () => {
-  fs.cp('./src/fs/files', './src/fs/files_copy', { recursive: true })
+const copy = async () => {
+  try {
+    await access('./src/fs/files')
+    try {
+      await cp('./src/fs/files', './src/fs/files_copy', {
+        recursive: true,
+        force: false,
+        errorOnExist: true,
+      })
+    } catch {
+      throw new Error('FS operation failed')
+    }
+  } catch {
+    throw new Error('FS operation failed')
+  }
 }
+
+await copy()
