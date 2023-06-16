9b5f623632d27b12bdfb6110ec68ff8b2fc0400f
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-16 14:21:57 +0300

    refactor: remove nested try/catch

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,15 +3,11 @@ import { cp, access } from 'fs/promises'
 const copy = async () => {
   try {
     await access('./src/fs/files')
-    try {
-      await cp('./src/fs/files', './src/fs/files_copy', {
-        recursive: true,
-        force: false,
-        errorOnExist: true,
-      })
-    } catch {
-      throw new Error('FS operation failed')
-    }
+    await cp('./src/fs/files', './src/fs/files_copy', {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    })
   } catch {
     throw new Error('FS operation failed')
   }
