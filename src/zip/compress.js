4d791f9cf941c490fb9d1b98a93831cbf836a2d1
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-16 14:12:28 +0300

    refactor: compress draft

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -6,7 +6,15 @@ const ws = fs.createWriteStream('./src/zip/files/archive.gz')
 const gzip = zlib.createGzip()
 
 const compress = async () => {
-  rs.pipe(gzip).pipe(ws)
+  const stream = rs.pipe(gzip).pipe(ws)
+  return await new Promise(resolve => stream.on("finish", resolve));
+}
+
+const deleteSourceFile = async () => {
+  fs.unlink('./src/zip/files/fileToCompress.txt', (err) => {
+    err ? console.log(err) : null
+  })
 }
 
 await compress()
+await deleteSourceFile()
