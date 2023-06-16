af95a4edb58167e9140294f974f23bf239b9f9d7
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2023-06-16 15:01:24 +0300

    feat: add source file/archive removal after compressing/decompressing

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { unlink } from 'fs/promises'
 import zlib from 'zlib'
 
 const rs = fs.createReadStream('./src/zip/files/fileToCompress.txt')
@@ -7,13 +8,15 @@ const gzip = zlib.createGzip()
 
 const compress = async () => {
   const stream = rs.pipe(gzip).pipe(ws)
-  return await new Promise(resolve => stream.on("finish", resolve));
+  return await new Promise((resolve) => stream.on('finish', resolve))
 }
 
 const deleteSourceFile = async () => {
-  fs.unlink('./src/zip/files/fileToCompress.txt', (err) => {
-    err ? console.log(err) : null
-  })
+  try {
+    await unlink('./src/zip/files/fileToCompress.txt')
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 await compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { unlink } from 'fs/promises'
 import zlib from 'zlib'
 
 const rs = fs.createReadStream('./src/zip/files/archive.gz')
@@ -6,7 +7,17 @@ const ws = fs.createWriteStream('./src/zip/files/fileToCompress.txt')
 const gzip = zlib.createUnzip()
 
 const decompress = async () => {
-  rs.pipe(gzip).pipe(ws)
+  const stream = rs.pipe(gzip).pipe(ws)
+  return await new Promise((resolve) => stream.on('finish', resolve))
+}
+
+const deleteSourceArch = async () => {
+  try {
+    await unlink('./src/zip/files/archive.gz')
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 await decompress()
+await deleteSourceArch()
