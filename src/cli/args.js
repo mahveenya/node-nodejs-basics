f4853c1847080166d81ec004b2794aba5441eecd
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-01-02 15:33:50 +0200

    refactor: remove process import

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,3 @@
-import process from 'process'
-
 const parseArgs = () => {
   for (let i = 2; i < process.argv.length; i += 2) {
     const propName = process.argv[i].replace('--', '')
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,3 @@
-import process from 'process'
-
 const parseEnv = () => {
   for (const [key, value] of Object.entries(process.env)) {
     if (key.includes('RSS_')) console.log(`${key}=${value}`)
