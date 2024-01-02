78d38ba2ae0970e1bc79f64edef49394b63986b8
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-01-02 15:35:32 +0200

    refactor: refactor read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -2,8 +2,8 @@ import { readFile } from 'fs/promises'
 
 const read = async () => {
   try {
-    const fileData = await readFile('./src/fs/files/fileToRead.txt', { flag: 'r' })
-    console.log(fileData.toString())
+    const fileData = await readFile('./src/fs/files/fileToRead.txt', { encoding: 'utf8' })
+    console.log(fileData)
   } catch {
     throw new Error('FS operation failed')
   }
