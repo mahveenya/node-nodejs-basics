d6092f3eae1d33b934840635e7b8222d018420f8
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-01-08 21:47:34 +0200

    refactor: use Promise.all instead of experimental cp func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,15 +1,33 @@
-import { cp, access } from 'fs/promises'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { access, copyFile } from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const copy = async () => {
   try {
-    await access('./src/fs/files')
-    await cp('./src/fs/files', './src/fs/files_copy', {
-      recursive: true,
-      force: false,
-      errorOnExist: true,
-    })
-  } catch {
-    throw new Error('FS operation failed')
+    const filesFolder = join(__dirname, 'files')
+    const filesCopyFolder = join(__dirname, 'files_copy')
+
+    const filesFolderDoesntExist = (await access(filesFolder)) !== undefined
+    if (filesFolderDoesntExist) throw new Error("files folder doesn't exist")
+
+    const filesCopyFolderExists = (await access(filesCopyFolder)) === undefined
+    if (filesCopyFolderExists)
+      throw new Error('files_copy folder already exists')
+
+    await mkdir(filesCopyFolder)
+    const filesToCopy = await readdir(filesFolder)
+
+    await Promise.all(
+      filesToCopy.map(
+        async file =>
+          await copyFile(join(filesFolder, file), join(filesCopyFolder, file)),
+      ),
+    )
+  } catch (e) {
+    throw new Error(`FS operation failed: ${e.message}`)
   }
 }
 
