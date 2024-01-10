fbf63aba04e68d6a9e9c22c100dcb30e781ca200
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-01-10 23:56:59 +0200

    refactor: use proper path

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,10 +1,16 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 import { writeFile } from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const text = 'I am fresh and young'
 
 const create = async () => {
   try {
-    await writeFile('./src/fs/files/fresh.txt', text, { flag: 'ax' })
+    const newFilePath = join(__dirname, 'files', 'fresh.txt')
+    await writeFile(newFilePath, text, { flag: 'ax' })
   } catch {
     throw new Error('FS operation failed')
   }
