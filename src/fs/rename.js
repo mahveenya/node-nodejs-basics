624e46249c8040266056aad5cc77716dee8de65a
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-01-10 15:18:11 +0200

    refactor: use dirname and filename in rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,10 +1,17 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 import fs from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const rename = async () => {
   try {
-    await fs.rename('./src/fs/files/wrongFilename.txt', './src/fs/files/properFilename.md')
+    const wrongFilenamePath = join(__dirname, 'files', 'wrongFilename.txt')
+    const properFilenamePath = join(__dirname, 'files', 'properFilename.md')
+    await fs.rename(wrongFilenamePath, properFilenamePath)
   } catch {
-    throw new Error('FS operation failed')
+    throw new Error(`FS operation failed:\n${e.message}`)
   }
 }
 
