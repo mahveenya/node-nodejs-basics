fc2deb53bbdb2a832e06a43bffb026cc2022b78d
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-01-10 23:02:55 +0200

    refactor: use proper paths

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,10 +1,16 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 import { unlink } from 'fs/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const remove = async () => {
   try {
-    await unlink('./src/fs/files/fileToRemove.txt')
+    const fileToRemovePath = join(__dirname, 'files', 'fileToRemove.txt')
+    await unlink(fileToRemovePath)
   } catch {
-    throw new Error('FS operation failed')
+    throw new Error(`FS operation failed:\n${e.message}`)
   }
 }
 
