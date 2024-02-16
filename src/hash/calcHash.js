c0825d5b08bc027c0f3a2cafe4d7bb44175d1c9f
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-02-16 22:37:55 +0200

    refactor: refactor hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,16 +1,21 @@
+import { join } from 'path'
 import { readFile } from 'fs/promises'
-import crypto from 'crypto'
+import { __dirname } from '../__dirname.js'
 
 const calculateHash = async () => {
   try {
-    const fileData = readFile('src/hash/files/fileToCalculateHashFor.txt', {
+    const fileToCalculateHashForPath = join(
+      __dirname,
+      'hash/files/fileToCalculateHashFor.txt',
+    )
+    const fileData = readFile(fileToCalculateHashForPath, {
       encoding: 'utf8',
     })
     const hash = crypto.createHash('sha256')
     hash.update(fileData)
     console.log(hash.digest('hex'))
   } catch {
-    throw new Error('FS operation failed')
+    throw new Error(`FS operation failed:\n${e.message}`)
   }
 }
 
